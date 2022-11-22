fd9ba376c94db6020c6d41d56be0764b34313586
SSiik <sik_5571@naver.com>
2022-11-23 00:01:49 +0900

    add some code

--- src/main/java/com/example/back/repository/crosswalkJDbcRepository.java
+++ src/main/java/com/example/back/repository/crosswalkJDbcRepository.java
@@ -31,7 +31,7 @@ public class crosswalkJDbcRepository {
     }
 
     public List<cross> selectConditionCross(){
-        String SQL = "SELECT latitude,longitude FROM public.\"cross\" where \"cross.st_length_\" >= 70";
+        String SQL = "SELECT latitude,longitude FROM public.\"cross\" where CAST(st_length_ as FLOAT8) >= 70";
         List<cross> results = jdbcTemplate.query(SQL,new crossMapper());
         return results;
     }
